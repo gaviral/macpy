922b6395af3a71ec27834ef685916d0611de0449
Aviral Garg <aviral.garg@icloud.com>
2024-01-10 21:14:17 -0800

    feat: setup dpg

--- ai/aicontroller.py
+++ ai/aicontroller.py
@@ -1,5 +1,11 @@
 import logging
 
+import dearpygui.dearpygui as dpg
+
+
+def save_callback():
+    print("Save Clicked")
+
 
 class AI:
     ai_name: str
@@ -10,9 +16,28 @@ class AI:
         self.setup()
 
     def setup(self):
+        # Setup member variables
         self.ai_name = f"AI-{AI.ai_instance_id}"
+
+        # Setup utilities
         self.setup_log_controller()
+        self.setup_dpg()
+
         self.log_info("AI setup complete")
+
+    @staticmethod
+    def setup_dpg():
+        dpg.create_context()
+        dpg.create_viewport()
+        dpg.setup_dearpygui()
+        with dpg.window(label="Example Window"):
+            dpg.add_text("Hello world")
+            dpg.add_button(label="Save", callback=save_callback)
+            dpg.add_input_text(label="string")
+            dpg.add_slider_float(label="float")
+        dpg.show_viewport()
+        dpg.start_dearpygui()
+        dpg.destroy_context()
 
     def setup_log_controller(self):
         logging.basicConfig(level=logging.DEBUG)
@@ -25,7 +50,8 @@ class AI:
         console_log_controller.setFormatter(self.get_log_formatter())
         logging.getLogger().addHandler(console_log_controller)
 
-    def get_log_formatter(self):
+    @staticmethod
+    def get_log_formatter():
         return logging.Formatter(
             f"[%(asctime)s] [%(levelname)s] [%(name)s] [%(filename)s:%(lineno)d] [%(funcName)s] %(message)s",
             "%Y-%m-%d %H:%M:%S")
@@ -36,5 +62,6 @@ class AI:
         file_log_controller.setFormatter(self.get_log_formatter())
         logging.getLogger().addHandler(file_log_controller)
 
-    def log_info(self, message):
+    @staticmethod
+    def log_info(message):
         logging.info(message)
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from ai.aicontroller import AI
 
+
 if __name__ == '__main__':
     ai = AI()
